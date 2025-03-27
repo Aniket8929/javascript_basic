adb5ca108987894c985ca5b7787fe58ac9f07000
Aniket <aniketkumarrai271@gmail.com>
2025-03-27 10:23:43 +0530

    Add files via upload

---
+++ prime/app.js
@@ -0,0 +1,154 @@
+
+// 1 Declare a variable city and assign it a city name. Log it to the console.
+// ************************************
+// var cityname = "India";
+// let cityname2 = 'UK';
+// const cityname3 = `London`;
+// console.log(cityname);
+// console.log(cityname2);
+// console.log(cityname3);
+// ************************************
+
+// 2 Create two variables, a = 5 and b = 10, and log their sum.
+// const a = 5;
+// const b = 10;
+// console.log(a + b);
+
+// 3 Swap the values of two variables.
+// ***************
+// let value1 = 10;
+// let value2 = 20;
+// console.log("before swap value", value1);
+// console.log("before swap value", value2);
+// let temp = value1;
+// value1 = value2;
+// value2 = temp;
+// console.log("after swap value", value1);
+// console.log("after swap value", value2);
+// **************
+// Declare a const variable for your birth year and log it.
+
+// const birthdate = "2 / 10 / 2005";
+// console.log(birthdate);
+//  ===========================Conditions & Loops:===================
+
+// Write a program that checks if a number is positive or negative.
+// let number = -10;
+// if (number > 0) {
+//   console.log("positive number");
+// }else if(number == 0){
+//   console.log("number is zero");
+// }else{
+//   console.log("negative number");
+// }
+
+// Check if a given number is divisible by 5 and 3.
+// const number = "15";
+
+// if (number % 5 == 0 && number % 3 == 0) {
+//   console.log("given number is divisible by 5 and 3");
+// } else {
+//   console.log("given number is not  divisible by 5 and 3");
+// }
+
+// const personage = 16;
+// if (personage >= 18) {
+//   console.log("Eligible for vote");
+// } else {
+//   console.log("not Eligible  for vote")
+// }
+// const character = "u";
+// const characterlowcase = character.toLowerCase();
+
+// if (characterlowcase == "a" || characterlowcase == "e" || characterlowcase == "i" || characterlowcase == "o" || characterlowcase == "u") {
+//   console.log("Vowel");
+
+// } else {
+//   console.log("Consonant");
+// }
+
+
+
+// let number1 = 10;
+// let number2 = 2;
+// let number3 = 8;
+
+// if (number1 > number2 && number1 > number3) {
+//   console.log(`number is ${number1} largest than ${number2}, ${number3}`);
+// } else if (number2 > number1 && number2 > number3) {
+//   console.log(`number is ${number2} largest than ${number1}, ${number3}`);
+// }
+// else {
+//   console.log(`number is ${number3} largest than ${number2}, ${number3}`);
+// }
+
+// let str = "abcdefghi"
+// let serchvalue = "x"
+// if (str.includes(serchvalue)) {
+//   console.log(`Contains ${serchvalue}`);
+// }
+// else {
+//   console.log(`Does not contain ${serchvalue}`);
+// }
+
+// let range = 5;
+// if (range >= 10 && range <= 50) {
+//   console.log("Within range");
+// }
+// else {
+//   console.log("Out of range");
+// }
+
+// let password = "holl3";
+// if(password.length >= 8 && /\d/.test(password)){
+//   console.log("Strong password");
+// }else{
+//   console.log("Weak password");
+// }
+
+
+// Print Numbers: Write a for loop to print numbers from 1 to 10 in the console.
+// let sum = 0;
+// for (let index = 0; index <= 10; index++) {
+//   sum = sum + index;
+// }
+// console.log(sum);
+
+// let num = 10;  // Start from 10
+
+// while (num >= 1) {  
+//     console.log(num);  // Print the number
+//     num--;  // Decrease the number by 1
+// }
+
+// *********************Array****************
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum = sum + numbers[i];
+// }
+// console.log(sum);
+// let max = numbers[0];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i]
+//   }
+// }
+
+// console.log(max);
+// let min = numbers[0];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] < min) {
+//     min = numbers[i]
+//   }
+// }
+
+// console.log(min);
+
+
+
+
+
+
+
+
